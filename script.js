2b5df1d44daf77ecb3ef3297385353a4e86ed872
DG Gueye <dgueye12@ClasswocessMini.hsd1.ga.comcast.net>
2022-08-01 15:24:40 -0400

    create todo

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 const BASE_URL = 'http://localhost:3000';
 const todopath = 'todos';
 const getPosts = () => {
-  const postsEndPoint = [BASE_URL, postsPath].join('/');
+  const postsEndPoint = [BASE_URL, todopath].join('/');
   return fetch(postsEndPoint).then((response) => {
     return response.json();
   });
 };
 
 const deletePost = (id) => {
-  const postsEndPoint = [BASE_URL, postsPath, id].join('/');
+  const postsEndPoint = [BASE_URL, todopath, id].join('/');
   return fetch(postsEndPoint, {
     method: 'DELETE',
   }).then((response) => {
@@ -17,7 +17,7 @@ const deletePost = (id) => {
   });
 };
 const updatePost = (id) => {
-  const postsEndPoint = [BASE_URL, postsPath, id].join('/');
+  const postsEndPoint = [BASE_URL, todopath, id].join('/');
   return fetch(postsEndPoint, {
     method: 'PUT',
   }).then((response) => {
@@ -25,11 +25,33 @@ const updatePost = (id) => {
   });
 };
 const patchPost = (id) => {
-  const postsEndPoint = [BASE_URL, postsPath, id].join('/');
+  const postsEndPoint = [BASE_URL, todopath, id].join('/');
   return fetch(postsEndPoint, {
     method: 'PATCH',
   }).then((response) => {
     return response.json();
   });
 };
+
+//Create Posts
+const createPosts = () => {
+    const form = document.querySelector("todo_form");
+    form.addEventListener('submit', function(e){
+    const postsEndPoint = [BASE_URL, todopath].join('/');
+    return fetch(postsEndPoint, {
+      method: 'POST',
+      body: JSON.stringify({
+          title:textbox,
+          completed:completed
+      })
+    })
+  });
+  };
+
+//Retrieve Posts
+
+const retrieve_posts = getPosts()
+    .then(json => {
+        console.log(json);
+    })
 
