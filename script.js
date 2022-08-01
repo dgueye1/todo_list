13a1155fd4f9f465e685bd51c7bd017bd19f51b0
DG Gueye <dgueye12@ClasswocessMini.hsd1.ga.comcast.net>
2022-08-01 17:28:33 -0400

    thid commit

--- script.js
+++ script.js
@@ -3,14 +3,7 @@ const BASE_URL = 'http://localhost:3000';
 const todopath = 'todos';
 
 
-const deletePost = (id) => {
-  const postsEndPoint = [BASE_URL, todopath, id].join('/');
-  return fetch(postsEndPoint, {
-    method: 'DELETE',
-  }).then((response) => {
-    return response.json();
-  });
-};
+
 
 const patchPost = (id) => {
   const postsEndPoint = [BASE_URL, todopath, id].join('/');
@@ -45,7 +38,9 @@ const getPosts = () => {
         const posts = json.map(post =>
         {
             return`
-                <div class="todo_title">${post.title}</div> 
+                <p class="todo_title">${post.title}</p>
+                <button type="button" class="edit_button"></button>
+                <button type="button" class="delete_button" value="delete"></button> 
                   `
         }).join("");
         document.querySelector(".todos").innerHTML = posts;
@@ -56,13 +51,43 @@ const getPosts = () => {
 
   //Updating Posts
   const updatePost = (id) => {
-    update_button = document.querySelector()
+    const update_button = document.querySelector(".edit_button");
+    update_button.addEventListener('click', () =>{
     const postsEndPoint = [BASE_URL, todopath, id].join('/');
     return fetch(postsEndPoint, {
       method: 'PUT',
-      body
+      body:JSON.stringify
     }).then((response) => {
       return response.json();
+    })
+    .then(json =>{
+        return `
+        <input type="text" name="change_title" id="change_title" placeholder="${json.title}">
+        `
+    })
     });
   };
+
+  //Delete Post
+  const deletePost = (id) => {
+    const postsEndPoint = [BASE_URL, todopath, id].join('/');
+    return fetch(postsEndPoint, {
+      method: 'DELETE',
+    }).then((response) => {
+      return response.json();
+    })
+  };
+
+  const deleteall = (posts) =>{
+    posts.addEventListener('click', (event) => {
+        if (name?.startsWith('delete_button')) {
+          const id = +name.substring(11);
+          deletePost(id).then(() => {
+            state.posts = state.posts.filter((post) => {
+              return post.id !== id;
+            });
+          });
+        }
+      });
+    };
 
