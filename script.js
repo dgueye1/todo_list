1f3f17b3d34319ea97f139a3cff5c42ee5cd60b6
DG Gueye <dgueye12@ClasswocessMini.hsd1.ga.comcast.net>
2022-08-01 16:09:51 -0400

    second commit

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
 //API calls
 const BASE_URL = 'http://localhost:3000';
 const todopath = 'todos';
-const getPosts = () => {
-  const postsEndPoint = [BASE_URL, todopath].join('/');
-  return fetch(postsEndPoint).then((response) => {
-    return response.json();
-  });
-};
+
 
 const deletePost = (id) => {
   const postsEndPoint = [BASE_URL, todopath, id].join('/');
@@ -16,14 +11,7 @@ const deletePost = (id) => {
     return response.json();
   });
 };
-const updatePost = (id) => {
-  const postsEndPoint = [BASE_URL, todopath, id].join('/');
-  return fetch(postsEndPoint, {
-    method: 'PUT',
-  }).then((response) => {
-    return response.json();
-  });
-};
+
 const patchPost = (id) => {
   const postsEndPoint = [BASE_URL, todopath, id].join('/');
   return fetch(postsEndPoint, {
@@ -35,23 +23,46 @@ const patchPost = (id) => {
 
 //Create Posts
 const createPosts = () => {
-    const form = document.querySelector("todo_form");
+    const form = document.querySelector(".todo_form");
     form.addEventListener('submit', function(e){
     const postsEndPoint = [BASE_URL, todopath].join('/');
     return fetch(postsEndPoint, {
       method: 'POST',
       body: JSON.stringify({
           title:textbox,
-          completed:completed
+          completed:Boolean(completed)
       })
     })
   });
   };
 
 //Retrieve Posts
+const getPosts = () => {
+    const postsEndPoint = [BASE_URL, todopath].join('/');
+    return fetch(postsEndPoint).then(response => 
+      response.json()
+    ).then(json => {
+        const posts = json.map(post =>
+        {
+            return`
+                <div class="todo_title">${post.title}</div> 
+                  `
+        }).join("");
+        document.querySelector(".todos").innerHTML = posts;
+    });
+  };
 
-const retrieve_posts = getPosts()
-    .then(json => {
-        console.log(json);
-    })
+  getPosts();
+
+  //Updating Posts
+  const updatePost = (id) => {
+    update_button = document.querySelector()
+    const postsEndPoint = [BASE_URL, todopath, id].join('/');
+    return fetch(postsEndPoint, {
+      method: 'PUT',
+      body
+    }).then((response) => {
+      return response.json();
+    });
+  };
 
